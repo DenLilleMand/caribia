18420b17702c5328abdd09791e0ce8f83b4397fd
Matti andreas nielsen <mattinielsen5@hotmail.com>
2016-07-10 16:15:33 +0200

    herp

--- wp-content/themes/caribia/functions.php
+++ wp-content/themes/caribia/functions.php
@@ -1,11 +1,14 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 
-function my_theme_enqueue_styles() {
+
+add_action( 'wp_enqueue_scripts', 'caribia_enqueue_styles', 12);
+function caribia_enqueue_styles()
+{
     $parent_style = 'parent-style';
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css', array( $parent_style )
-    );
+    wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css');
+    wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', array($parent_style));
+    wp_enqueue_style('caribia-css', get_stylesheet_directory_uri() . '/css/caribia.css');
+    wp_enqueue_script('caribia-js', get_stylesheet_directory_uri() . '/js/caribia.js', array('jquery'));
 }
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+
