24b6f3c68957507103ef13010300c60a7d63db6d
Matti andreas nielsen <mattinielsen5@hotmail.com>
2016-07-11 23:59:01 +0200

    intro blok forste udkast

--- wp-content/themes/caribia/widgets/intro.php
+++ wp-content/themes/caribia/widgets/intro.php
@@ -48,8 +48,27 @@ class Intro_Widget extends WP_Widget {
      * @param array $instance Saved values from database.
      */
     public function widget($args, $instance) {
+        $title = $instance['title'];
+        $top_image_url = $instance['top_image_url'];
+        $bottom_image_url = $instance['bottom_image_url'];
+        $description = $instance['description'];
         echo $args['before_widget'];
-        echo "herpderp";
+        ?>
+            <div class="intro-widget-container">
+                <div class="header">
+                    <img class="img-responsive top-image" src="<?php echo $top_image_url ?>" >
+                    <img class="img-responsive bottom-image" src="<?php echo $bottom_image_url ?>" >
+                    <h2><?php echo $title ?></h2>
+                </div>
+                <div class="separator"></div>
+                <div class="body">
+                    <p>
+                        <?php echo $description ?>
+                    </p>
+                </div>
+            </div>
+
+        <?php
         echo $args['after_widget'];
     }
 
@@ -59,7 +78,9 @@ class Intro_Widget extends WP_Widget {
      */
     public function form($instance) {
         $title = !empty( $instance['title'] ) ? $instance['title'] : __( 'New title', 'text_domain' );
-        $image_url = !empty($instance['image_url']) ? $instance['image_url'] : __('New imageurl', 'text_domain');
+        $top_image_url = !empty($instance['top_image_url']) ? $instance['top_image_url'] : __('New top image url', 'text_domain');
+        $bottom_image_url = !empty($instance['bottom_image_url']) ? $instance['bottom_image_url'] : __('New bottom image url', 'text_domain');
+        $description = !empty( $instance['description'] ) ? $instance['description'] : __( 'New description', 'text_domain' );
         ?>
         <p>
             <label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php _e( esc_attr( 'Title:' ) ); ?></label>
@@ -67,10 +88,19 @@ class Intro_Widget extends WP_Widget {
                    name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
         </p>
         <p>
-            <label for="<?php echo esc_attr( $this->get_field_id( 'image_url' ) ); ?>"><?php _e( esc_attr( 'Image url:' ) ); ?></label>
-            <input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'image_url' ) ); ?>"
-                   name="<?php echo esc_attr( $this->get_field_name( 'image_url' ) ); ?>" type="text" value="<?php echo esc_attr( $image_url ); ?>">
-            <input type="button" name="upload-btn" id="upload-btn" class="button-secondary" value="Upload Image">
+            <label for="<?php echo esc_attr( $this->get_field_id('top_image_url')); ?>"><?php _e(esc_attr('Top image url:')); ?></label>
+            <input class="widefat" id="<?php echo esc_attr($this->get_field_id('top_image_url')); ?>" name="<?php echo esc_attr( $this->get_field_name('top_image_url')); ?>" type="text" value="<?php echo esc_attr($top_image_url); ?>">
+            <input type="button" name="upload-btn" id="upload-btn-top-image" class="button-secondary image-file-upload" value="Upload Image">
+        </p>
+        <p>
+            <label for="<?php echo esc_attr($this->get_field_id('bottom_image_url')); ?>"><?php _e(esc_attr('Bottom image url:')); ?></label>
+            <input class="widefat" id="<?php echo esc_attr($this->get_field_id('bottom_image_url')); ?>" name="<?php echo esc_attr( $this->get_field_name('bottom_image_url') ); ?>" type="text" value="<?php echo esc_attr($bottom_image_url); ?>">
+            <input type="button" name="upload-btn" id="upload-btn-bottom-image" class="button-secondary image-file-upload" value="Upload Image">
+        </p>
+        <p>
+            <label for="<?php echo esc_attr( $this->get_field_id( 'description' ) ); ?>"><?php _e( esc_attr( 'Description:' ) ); ?></label>
+            <input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'description' ) ); ?>"
+                   name="<?php echo esc_attr( $this->get_field_name( 'description' ) ); ?>" type="text" value="<?php echo esc_attr( $description ); ?>">
         </p>
         <?php
     }
@@ -86,10 +116,11 @@ class Intro_Widget extends WP_Widget {
      * @return array Updated safe values to be saved.
      */
     public function update($new_instance, $old_instance) {
-        $instance = $old_instance;
-        $instance['title'] = strip_tags($new_instance['title']);
-        $instance['image_url'] = strip_tags($instance['image_url']);
-        $this->flush_widget_cache();
+        $instance = array();
+        $instance['title'] = !empty($new_instance['title']) ? strip_tags($new_instance['title']) : '';
+        $instance['top_image_url'] = !empty($new_instance['top_image_url']) ? strip_tags($new_instance['top_image_url']) : '';
+        $instance['bottom_image_url'] = !empty($new_instance['bottom_image_url']) ? strip_tags($new_instance['bottom_image_url']) : '';
+        $instance['description'] = !empty($new_instance['description']) ? strip_tags($new_instance['description']) : '';
         return $instance;
     }
 
