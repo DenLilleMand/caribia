c35550461dbe3e872c9d1525b32f20174e8292ca
Sjuften <martinbergpetersen@gmail.com>
2016-07-24 17:44:24 +0200

    navbar fix

--- wp-content/themes/caribia/js/caribia.js
+++ wp-content/themes/caribia/js/caribia.js
@@ -2,3 +2,12 @@ function getBlogPost(url){
    
   window.open(url,'_self');
 }
+function setNavbar($){
+var test = $('<li id="navbar-logo"> <div class="col-md-4 header-logo-container"><a class="header-logo" href="#" rel="home"><img id="navigation_menu_logo" class="navbar-brand float-none" src="http://localhost/caribia/wp-content/themes/caribia/images/logo.png"></img></a></div></li>');
+
+$("#menu-main-menu :nth-child(4)").after(test);
+
+}
+jQuery(document).ready(function($) {
+setNavbar($);
+});
