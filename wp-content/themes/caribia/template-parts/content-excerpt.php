f6f52d33614aaa5505fe8633b72cb8a194eee5bf
Matti andreas nielsen <mattinielsen5@hotmail.com>
2016-07-29 19:44:43 +0200

    herp

--- wp-content/themes/caribia/template-parts/content-excerpt.php
+++ wp-content/themes/caribia/template-parts/content-excerpt.php
@@ -5,8 +5,8 @@
  * @package Nisarg
  */
 ?>
-<div class="aeffect">
-<article  id="post-<?php the_ID(); ?>"  <?php post_class('col-md-4 aeffect'); ?>  style="cursor:pointer" onclick="getBlogPost('<?php the_permalink();?>')">
+
+<article  id="post-<?php the_ID(); ?>"  <?php post_class('col-md-4 aeffect'); ?>  onclick="getBlogPost('<?php the_permalink();?>')">
 
 	<?php
 	if (is_sticky() && is_home() && ! is_paged()) {
@@ -57,8 +57,7 @@
 				</div>	
 		</div>
 	</div><!-- .entry-summary -->
-	
-	
+
+
 </article>
 <!-- #post-## -->
-</div>
